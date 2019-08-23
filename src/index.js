7e55b696db9bb2457386a5f5520f80c30c50bba5
Aaditya Sahay <aadityasahay1@gmail.com>
2019-08-23 19:33:15 +0530

    AI working

--- src/index.js
+++ src/index.js
@@ -1,61 +1,67 @@
 let runners = [];
 let food = [];
-let poison = [];
 var population;
 var popmax;
-var mutation;
+var debug;
+var cloneTime, foodtime, mutationRate
 
 function setup() {
-    createCanvas(1600, 900);
-    for (let i = 0; i < 40; i++) {
+    createCanvas(1024, 768);
+    for (let i = 0; i < 100; i++) {
         var x = random(width)
         var y = random(height)
         food.push(createVector(x, y))
     }
-    for (let i = 0; i < 20; i++) {
+    mutationRate = {
+        slider: createSlider(),
+        text: createP("Mutation Rate")
+    }
+    popmax = 50
+    for (let i = 0; i < popmax; i++) {
         var x = random(width)
         var y = random(height)
-        poison.push(createVector(x, y))
+        runners[i] = new Blob(x, y);
     }
-    mutation = 0.1
-    popmax = 5
-    population = new Population(popmax, mutation)
-
+    debug = {
+        checkbox: createCheckbox(),
+    }
 }
 
-
-function keyPressed() {
-    if (keyCode === BACKSPACE) population.toggleDebug();
-}
 
 function draw() {
     background(255);
 
-    if (food.length < 40) {
+    if (random(1) < 0.08) {
         var x = random(width)
         var y = random(height)
         food.push(createVector(x, y))
     }
-    if (poison.length < 20) {
-        var x = random(width)
-        var y = random(height)
-        poison.push(createVector(x, y))
-    }
+
 
     for (const i in food) {
         let Food = food[i]
-        fill(0, 255, 0)
+        fill(0, 0, 255)
         noStroke()
-        ellipse(Food.x, Food.y, 10, 10)
+        ellipse(Food.x, Food.y, 4, 4)
     }
-    for (const i in poison) {
-        let Poison = poison[i]
-        fill(255, 0, 0)
-        noStroke()
-        ellipse(Poison.x, Poison.y, 10, 10)
+
+
+
+    for (let i = runners.length - 1; i >= 0; i--) {
+        runners[i].boundaries();
+        runners[i].behaviours(food, runners)
+        runners[i].update();
+        runners[i].display();
+        var newRunner = runners[i].cloning()
+        if (newRunner != null) {
+            runners.push(newRunner)
+        }
+        if (runners[i].isDead() == true) {
+            food.push(createVector(runners[i].position.x, runners[i].position.y))
+            runners.splice(i, 1)
+        }
     }
-    population.update()
-    population.isDead()
-    document.getElementsByTagName('h1')[0].innerHTML = "Best Score: " + this.population.record
+    var millisecond = millis()
+    text : text('Milliseconds \nrunning: \n' + millisecond, 5, 40)
 }
 
--- src/runner.js
+++ src/runner.js
@@ -1,23 +1,45 @@
-class Vehicle {
-    constructor(x, y) {
+var m = 0.15
+
+class Blob {
+    constructor(x, y, dna) {
         this.acceleration = createVector(0, 0);
         this.velocity = createVector(0, -2);
         this.position = createVector(x, y);
-        this.r = 6;
+        this.r = 8;
         this.maxspeed = 5;
         this.maxforce = 0.5;
         this.health = 1
-        this.dna = [random(-3, 3), random(-3, 3), random(10, 200), random(10, 200)];
-        this.fitness = 0;
+        this.dna = []
+        if (!dna) {
+            this.dna[0] = random(-3, 3)
+            this.dna[1] = random(-3, 3)
+            this.dna[2] = random(0, 200)
+            this.dna[3] = random(0, 200)
+        } else {
+            this.dna[0] = dna[0]
+            this.dna[1] = dna[1]
+            this.dna[2] = dna[2]
+            this.dna[3] = dna[3]
+            if (random(1) < m) {
+                this.dna[0] += random(-1, 1)
+            }
+            if (random(1) < m) {
+                this.dna[1] += random(-1, 1)
+            }
+            if (random(1) < m) {
+                this.dna[2] += random(-10, 10)
+            }
+            if (random(1) < m) {
+                this.dna[3] += random(-10, 10)
+            }
+        }
     }
     update() {
-        this.fitness++
         this.health -= 0.005;
+        this.r -= 0.005
         this.velocity.add(this.acceleration);
-
         this.velocity.limit(this.maxspeed);
         this.position.add(this.velocity);
-
         this.acceleration.mult(0);
     }
 
@@ -52,14 +74,14 @@ class Vehicle {
 
     behaviours(good, bad) {
 
-        var foodSteer = this.eatFood(good, 0.2, this.dna[2]);
-        var poisonSteer = this.eatFood(bad, -0.5, this.dna[3]);
+        var foodSteer = this.eatFood(good, 0.3, this.dna[2]);
+        var blobSteer = this.eatBlob(bad,  this.dna[3]);
 
         foodSteer.mult(this.dna[0])
-        poisonSteer.mult(this.dna[1])
+        blobSteer.mult(this.dna[1])
 
         this.applyForce(foodSteer);
-        this.applyForce(poisonSteer);
+        this.applyForce(blobSteer);
     }
     seek(target) {
         var desired = p5.Vector.sub(target, this.position);
@@ -68,98 +90,86 @@ class Vehicle {
         steer.limit(this.maxforce);
         return steer
     }
-    createChild(best, m) {
-        var x = random(width)
-        var y = random(height)
-        var child = new Vehicle(x, y)
-        for (let i in child.dna) {
-            child.dna[i] = best.dna[i]
-        }
-        child.mutate(m)
-        return child;
-    }
-    mutate(m) {
-        for (let i in this.dna) {
-            if (random(1) < m) {
-                if (i > 1) {
-                    this.dna[i] = random(10, 200)
-                }
-                else {
-                    this.dna[i] = random(-3, 3)
-                }
-            }
-        }
-    }
-    eatFood(list, change, perception) {
-        var closestIndex = -1;
-        var record = Infinity;
-        for (let i in list) {
-            var d = this.position.dist(list[i])
-            if (perception > d && d < record) {
-                if (d < record) {
-                    record = d
-                    closestIndex = i
-                }
-            }
-        }
 
-        if (record < 5) {
-            list.splice(closestIndex, 1)
-            this.health += change;
-            this.fitness += change * 100;
-        } else if (closestIndex > -1) {
-            return this.seek(list[closestIndex])
+    eatFood(list, change, perception) {
+        var closestIndex = null;
+        var record = Infinity;
+        for (let i = list.length - 1; i >= 0; i--) {
+            var d = this.position.dist(list[i])
+            if (d < this.maxspeed) {
+                list.splice(i, 1)
+                this.health += change;
+                var sum = PI * this.r * this.r + PI * 16;
+                this.r = sqrt(sum / PI);
+            } else if (d < perception && d < record) {
+                record = d
+                closestIndex = list[i]
+            }
+
+        }
+        if (closestIndex != null) {
+            return this.seek(closestIndex)
+        }
+        return createVector(0, 0)
+    }
+
+    eatBlob(blobs, perception) {
+        var closest = null;
+        var record = Infinity;
+        for (let i = blobs.length - 1; i >= 0; i--) {
+            var d = this.position.dist(blobs[i].position)
+            if ( d < this.r + blobs[i].r) {
+                if (this.r > blobs[i].r) {
+                    var sum = PI * pow(this.r, 2) + PI * pow(blobs[i].r, 2);
+                    this.r = sqrt(sum / PI);
+                    blobs[i].health -= blobs[i].health
+                }else if (d < perception && d < record) {
+                    record = d
+                    closest = blobs[i].position
+                }
+            }
+        }
+        if (closest != null) {
+            return this.seek(closest)
         }
 
         return createVector(0, 0)
     }
-
-
     isDead() {
         return (this.health < 0)
     }
 
-
-    getFitness() {
-        return (this.fitness)
+    cloning() {
+        if (random(1) < 0.002) {
+            var x = random(width)
+            var y = random(height)
+            return new Blob(x, y, this.dna)
+        } else {
+            return null;
+        }
     }
-
-
-    showDebug() {
-        let theta = this.velocity.heading() + PI / 2;
-        translate(this.position.x, this.position.y);
-        rotate(theta)
-        stroke(0, 255, 0)
-        noFill()
-        line(0, 0, 0, -this.dna[0] * 10)
-        ellipse(0, 0, this.dna[2] * 2, this.dna[2] * 2)
-        stroke(255, 0, 0)
-        line(0, 0, 0, -this.dna[1] * 10)
-        ellipse(0, 0, this.dna[3] * 2, this.dna[3] * 2)
-
-    }
-
-
     display() {
-        // Draw a triangle rotated in the direction of velocity
         let theta = this.velocity.heading() + PI / 2;
-
         push();
         translate(this.position.x, this.position.y);
         rotate(theta);
-
+        if (debug.checkbox.checked()) {
+            stroke(0, 255, 0)
+            noFill()
+            line(0, 0, 0, -this.dna[0] * 10)
+            ellipse(0, 0, this.dna[2] * 2, this.dna[2] * 2)
+            stroke(255, 0, 0)
+            line(0, 0, 0, -this.dna[1] * 10)
+            ellipse(0, 0, this.dna[3] * 2, this.dna[3] * 2)
+        }
         var gr = color(0, 255, 0)
         var rd = color(255, 0, 0)
         var actual = lerpColor(rd, gr, this.health)
-
         fill(actual);
         stroke(200);
         strokeWeight(1);
         beginShape();
-
-        vertex(0, -this.r * 2);
-        vertex(-this.r, this.r * 2);
-        vertex(this.r, this.r * 2);
+        ellipse(0, 0, this.r, this.r)
         endShape(CLOSE);
         pop();
     }
